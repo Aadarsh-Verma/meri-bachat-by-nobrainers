31719fe17735784f9b5f6f8e342b8ec0ee967bf0
AadarshVerma <aadarshverma36@gmail.com>
2021-05-22 00:31:47 +0530

    fixed minor bugs

--- MeriBachat/urls.py
+++ MeriBachat/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
@@ -21,7 +23,8 @@ from src.views import home, index, compare, compare_util
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home,name='home'),
-    path('index/<pk>',index,name='index'),
-    path('compare/<name>',compare_util,name='compare'),
+    path('', home, name='home'),
+    path('index/<pk>', index, name='index'),
+    path('compare/<name>', compare_util, name='compare'),
 ]
+urlpatterns += [] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/models.py
+++ src/models.py
@@ -6,13 +6,13 @@ from django.db import models
 class Mobile(models.Model):
     category = models.CharField(max_length=50)
     name = models.CharField(max_length=255)
-    price = models.CharField(max_length=10)
-    rating = models.CharField(max_length=3)
-    specs = models.TextField()
-    description = models.TextField()
-    images = models.TextField()
-    reviews = models.TextField()
-    product_url = models.URLField()
+    price = models.CharField(max_length=10,blank=True,null=True)
+    rating = models.CharField(max_length=3,blank=True,null=True)
+    specs = models.TextField(blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    images = models.TextField(blank=True,null=True)
+    reviews = models.TextField(blank=True,null=True)
+    product_url = models.URLField(blank=True,null=True)
 
     def __str__(self):
         return self.name
